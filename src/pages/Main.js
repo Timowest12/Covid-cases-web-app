ee3723f6c2b94fdbbf982f263261d5f87280a589
Timowest12 <timowester@gmail.com>
2022-01-04 09:50:44 +0100

    Add main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import { getAllData } from '../API'
+import { useDispatch} from "react-redux";
 
 const Main = () => {
+    const dispatch = useDispatch();
+    const action = {
+        type: 'ADD_TO_CART',
+        payload: {
+          product: 'margarine',
+          quantity: 4
+        }
+      }
+      dispatch(action)
     return (
         <div>
             hello
