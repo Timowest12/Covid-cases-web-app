9130442e8be1daea2eac44be25b3f5da7531bea1
Timowest12 <timowester@gmail.com>
2022-01-05 13:37:23 +0100

    Display data on main page

--- src/components/MainBlock.js
+++ src/components/MainBlock.js
@@ -3,7 +3,11 @@ import React from 'react'
 const MainBlock = (props) => {
     return (
         <div className='mainCountryBlock'>
+            <div>
             {props.obj.Country}
+            
+            </div>
+            <img className='countryImg' src={`/all/${props.obj.CountryCode.toLowerCase()}/vector.svg`} alt="" />
         </div>
     )
 }
