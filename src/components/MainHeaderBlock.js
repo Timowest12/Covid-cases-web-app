cbac6ce008ad82fbdd852931b145adf011d00dad
Timowest12 <timowester@gmail.com>
2022-01-05 22:33:27 +0100

    Add styling

--- src/components/MainHeaderBlock.js
+++ src/components/MainHeaderBlock.js
@@ -5,7 +5,8 @@ const MainHeaderBlock = () => {
         <div className='mainHeaderBlock'>
             <div className="mainHeaderBlockLeft"><img src='/worldMap.svg' /></div>
             <div className="mainHeaderBlockRight">
-                Covid stats by country
+                Worldwide data:
+                
             </div>
         </div>
     )
