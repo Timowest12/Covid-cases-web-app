30f2e6894319672db5d557aafd2f5e9c778fd1b8
Timowest12 <timowester@gmail.com>
2022-01-06 01:17:05 +0100

    refactor update function

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,6 +8,7 @@ import MainHeaderBlock from '../components/MainHeaderBlock';
 const Main = () => {
     const dispatch = useDispatch();
    const [countryState, setCountryState] = useState('TotalConfirmed')
+   const [reverse, setReverse] = useState(false)
    //const [metric, setMetric] = useState('TotalConfirmed')
    const [countriesupdated, setcountriesupdated] = useState(false)
    if (countriesupdated == true) {
@@ -24,18 +25,31 @@ const Main = () => {
       
       const changeMetric = (e) => {
        let metric = e.target.value
-          countries = countries.sort((a,b) => b[metric] - a[metric])
-          setcountriesupdated(metric)
+       
+       setcountriesupdated(metric)
+      updateList()
+    }
+    const updateList = () => {
+        countries = countries.sort((a,b) => b[countriesupdated] - a[countriesupdated])
+    }
+      const changeOrder = (e) => {
+        countries = countries.reverse()
+        changeMetric(e.target.value)
     }
     return (
         <div>
             <MainHeaderBlock />
             <div className="indicationStripe">
-                stats sorted by: <select onChange={changeMetric} name="" id="">
+                stats sorted by: 
+                <select onChange={changeMetric} name="" id="">
                     <option value="TotalConfirmed">Total cases</option>
                     <option value="TotalDeaths">Total deaths</option>
                     <option value="NewConfirmed">New cases</option>
                     <option value="NewDeaths">New deaths</option>
+                </select>
+                <select onChange={changeOrder} name="" id="">
+                    <option value='normal'>high to low</option>
+                    <option value='reversed'>Low to high</option>
                 </select>
             </div>
             <div className='mainCountryWrapper'>
