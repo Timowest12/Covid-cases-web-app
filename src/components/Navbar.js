ea55da13fa114c051ac416bf06363521e106cbab
Timowest12 <timowester@gmail.com>
2022-01-06 02:44:53 +0100

    Add back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { BsChevronLeft } from 'react-icons/bs';
 
 const Navbar = (props) => {
     return (
         <nav>
-            <div className="left">back</div>
+            <div className="left"><BsChevronLeft /></div>
             <div className="right">{props.page}</div>
             <div className="right">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
         </nav>
